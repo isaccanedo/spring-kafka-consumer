7f90d3e017dff8996f2b27b14da32ff73ac97c7c
isaccanedo <isaccanedo@gmail.com>
2022-03-03 21:13:18 -0300

    Initial commit

---
+++ src/main/java/org/matias/springkafka/Consumer.java
@@ -0,0 +1,18 @@
+package org.matias.springkafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(id = "${spring.kafka.consumer.group-id}", 
+               topics = "${spring.kafka.consumer.topics}")
+public class Consumer {
+
+    @KafkaHandler(isDefault = true)
+    public void consume(ConsumerRecord<String, String> message) {
+        System.out.println("Consumed key=" + message.key() + ", value=" + message.value());
+    }
+}
